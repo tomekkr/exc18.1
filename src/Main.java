2f840fe7f7893534ad8ea621f2d20feac7b852de
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-06-07 18:51:38 +0200

    rozwiązanie v2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Main {
     public static void main(String[] args) {
-        List<Integer> numbersList = Stream.iterate(8, x -> x + 1)
-                .filter(x -> x > 100 && x % 5 == 0)
+        Stream.iterate(8, x -> x + 1)
+                .filter(x -> x > 100)
+                .filter(x -> x % 5 == 0)
                 .map(x -> x * 3)
                 .limit(10)
-                .collect(Collectors.toList());
-
-        System.out.println(numbersList);
+                .forEach(System.out::println);
     }
 }
